71bfbe6b9cbb85c4016a2e952465721fd49d3ff2
pashock5530 <pashock5530@gmail.com>
2016-02-18 00:27:07 +0200

    18.02.16 home work lesson 9

--- lesson9/app/src/main/java/com/example/pashock/lesson9/MainActivity.java
+++ lesson9/app/src/main/java/com/example/pashock/lesson9/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.pashock.lesson9;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,68 +15,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //temp0(0);
-        //temp1(0);
-        //temp2(1);
     }
 
     public void onClick(View view) {
-        temp0(0);
-        temp1(1);
-        temp2(5);
-    }
-
-    //перегрузка
-    int calc(int a) {
-        return a + 1;
-    }
-
-    int calc(int a, int b) {
-        return a + b;
-    }
-
-    //рекурсия
-    void recurs(int i) {
-        Log.d("recurs", String.valueOf(i));
-        if (i == 3) {
-            return;
-        } else {
-            recurs(i+=1);
-        }
-        Log.d("recurs", String.valueOf(i));
-    }
-
-    //%2
-    void temp0(int i) {
-        if (i == 10) {
-            return;
-        }
-        if (i % 2 != 0) {
-            Log.d("temp0", String.valueOf(i));
-        }
-        temp0(i+=1);
-    }
-
-    //%3
-    void temp1(int i) {
-        if (i == 10) {
-            return;
-        }
-        if (i % 3 == 0) {
-            Log.d("temp1", String.valueOf(i));
-        }
-        temp1(i+=1);
+        factorial(4);
     }
 
     //факториал
-    int temp2(int i) {
+    public int factorial(int i) {
         int a;
         if (i == 1) {
             a = 1;
         } else {
-            a = i * temp2(i-=1);
+            a = i * factorial(--i);
         }
-        Log.d("temp2", String.valueOf(a));
+        Log.d("factorial", String.valueOf(a));
         return a;
+    }
+
+    double kredSum = 0;
+    int period = 0;
+
+    public void onSumClick(View view) {
+        EditText fieldSum = (EditText) findViewById(R.id.fieldSum);
+        kredSum = Double.valueOf(fieldSum.getText().toString());
+        EditText fieldPeriod = (EditText) findViewById(R.id.fieldPeriod);
+        period = Integer.valueOf(fieldPeriod.getText().toString());
+        TextView textMSum = (TextView) findViewById(R.id.textMSum);
+        textMSum.append(" "+String.valueOf(kredSum/period)+" грн");
+    }
+
+    double periodSum = 0;
+    int sch = 1;
+
+    public void onPSumClick(View view) {
+        EditText fieldPSum = (EditText) findViewById(R.id.fieldPSum);
+        periodSum += Double.valueOf(fieldPSum.getText().toString());
+        TextView textPSum = (TextView) findViewById(R.id.textPeriodSum);
+        sch++;
+        textPSum.setText("Платеж "+String.valueOf(sch));
+        fieldPSum.setText("");
+        TextView textStatus = (TextView) findViewById(R.id.textStatus);
+        if (kredSum - periodSum > 0 && kredSum - periodSum < kredSum) {
+            textStatus.setText("Состояние: задолженость " + String.valueOf(kredSum - periodSum) + " грн");
+        } else if (kredSum - periodSum < 0) {
+            textStatus.setText("Состояние: переплата " + String.valueOf(-1 * (kredSum - periodSum)) + " грн");
+        } else {
+            textStatus.setText("Состояние: долг отсутствует");
+        }
     }
 }
