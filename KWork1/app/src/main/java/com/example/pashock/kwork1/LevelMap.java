67daab1bd7dc598bf45cec989d8a89cde695d625
pashock5530 <pashock5530@gmail.com>
2016-04-13 15:57:13 +0300

    kw2

--- KWork1/app/src/main/java/com/example/pashock/kwork1/LevelMap.java
+++ KWork1/app/src/main/java/com/example/pashock/kwork1/LevelMap.java
@@ -10,8 +10,8 @@ public class LevelMap implements ILevelMap {
     private HashMap<Integer, Level> map = new HashMap<>();
 
     public LevelMap() {
-        map.put(1, new Level(1, 5, 1,"mouse", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpeg"));
-        map.put(2, new Level(2, 10, 2, "word", "img1", "img2", "img3", "img4"));
+        map.put(1, new Level(1, 5, 1, "mouse", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpeg"));
+        map.put(2, new Level(2, 10, 2, "nokia", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"));
     }
 
     @Override
